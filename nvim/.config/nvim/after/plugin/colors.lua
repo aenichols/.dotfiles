cb40b17610970d2ea41ed05ce947a84d26b57794
Anthony Nichols <anthony.nichols@bngholdingsinc.com>
2023-01-12 15:09:13 -0600

    automagic messaging from me in the past.  Have you checked up your butthole?

--- nvim/.config/nvim/after/plugin/colors.lua
+++ nvim/.config/nvim/after/plugin/colors.lua
@@ -7,6 +7,9 @@ function ColorMyPencils(color)
 
     vim.api.nvim_set_hl(0, "Normal", { bg = "none" })
     vim.api.nvim_set_hl(0, "NormalFloat", { bg = "none" })
+
+    vim.api.nvim_set_hl(0, "ColorColumn", { bg = "#282828" })
+    vim.api.nvim_set_hl(0, "MatchParen", { fg = "black", bg = "#8ec07c", bold = true })
 end
 
 ColorMyPencils()
